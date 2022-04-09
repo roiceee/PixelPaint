05c35835c3c77e331f65449c7106d84c100b3bd1
jroiceee <jroicealdeza@gmail.com>
2022-04-10 03:22:11 +0800

    added final features

--- script.js
+++ script.js
@@ -27,49 +27,165 @@ function generateColors () {
     }
 }
 
-function generateCanvas(x ,num) {
-        calculateCellSize(x, num);
-    for (let i = 0; i < x; i++){
+
+
+function generateCanvas(gridValue ,containerSize) {
+        gridValue = parseInt(gridValue);
+        calculateCellSize(gridValue, containerSize);
+    for (let i = 0; i < gridValue; i++){
         const container = document.querySelector('.div-container');
         const div1 = document.createElement('div');
         div1.setAttribute('class', 'column');
         div1.setAttribute('style', `height: ${colHeight}px; width: ${colWidth}px`);
         container.append(div1);
-        for (let j= 0; j < x; j++) {
+        for (let j= 0; j < gridValue; j++) {
             const div2 = document.createElement('div');
             div2.setAttribute('class', 'row');
             div2.setAttribute('style', `height: ${rowHeight}px; width: ${rowWidth}px`);
             div1.append(div2);
         }
     }
+    divEventListener();
 }
+function calculateCellSize(gridValue, containerSize) {
+    colHeight = containerSize / gridValue;
+    colWidth = containerSize;
+    rowHeight = colHeight;
+    rowWidth = containerSize / gridValue;
+}
+
 function getContainerSize (){
-    let divSize = document.querySelector('.div-container').offsetHeight;
+    let divSize = document.querySelector('.div-container').offsetWidth;
     divSize = divSize - 4;
     console.log(divSize);
     return divSize;
 }
-function showRangeValue() {
+function getRangeValue() {
+        
         document.querySelector('.slider').oninput = function () {
-        gridValue = this.value;
-        this.style.background = `linear-gradient(to right, #ff7ee5 ${gridValue}%, white 50%`;
+        gridValue = parseInt(this.value);
+        y = this.value;
+        this.style.background = `linear-gradient(to right, #ff7ee5 ${y}%, white 50%`;
         const textValue = document.querySelector('.count');
-        textValue.textContent = gridValue + " x " + gridValue;
+        textValue.textContent = y + " x " + y;
+    }
+}
+function clearCanvas() {
+    const rows = document.querySelectorAll('.row');
+    const cols = document.querySelectorAll('.column');
+    rows.forEach((row) => {
+        row.remove();
+    })
+    cols.forEach((col) => {
+        col.remove();
+    })
+}
+function setWhite() {
+    const rows = document.querySelectorAll('.row');
+    const cols = document.querySelectorAll('.column');
+    rows.forEach((row) => {
+        row.style.backgroundColor = "white";
+    })
+    cols.forEach((col) => {
+        row.style.backgroundColor = "white";
+    })
+}
+
+function changeColor(e) {
+    //this will only take effect on mousehold
+    if (e.type === 'mouseover' && !mouseDown) return
+
+    switch(activeButton) {
+        case "black": e.target.style.backgroundColor = "var(--clr-1)"
+        break;
+        case "red": e.target.style.backgroundColor = "var(--clr-2)"
+        break;
+        case "blue": e.target.style.backgroundColor = "var(--clr-4)"
+        break;
+        case "green": e.target.style.backgroundColor = "var(--clr-5)"
+        break;
+        case "yellow": e.target.style.backgroundColor = "var(--clr-3)"
+        break;
+        case "pink": e.target.style.backgroundColor = "var(--clr-6)"
+        break;
+        case "rainbow": e.target.style.backgroundColor = `var(--clr-${Math.floor(Math.random() * 7) + 1})`
+        break;
+        case "white": e.target.style.backgroundColor = "var(--clr-8)"
+        break;
+    }
+}
+function setActiveButton(buttonName) {
+    switch(buttonName) {
+        case "black": activeButton = "black";
+        break;
+        case "red": activeButton = "red";
+        break;
+        case "blue": activeButton = "blue";
+        break;
+        case "green": activeButton = "green";
+        break;
+        case "yellow": activeButton = "yellow";
+        break;
+        case "pink": activeButton = "pink";
+        break;
+        case "rainbow": activeButton = "rainbow";
+        break;
+        case "white": activeButton = "white";
+        break;
     }
 }
 
-function calculateCellSize(x, num) {
-    colHeight = num / x;
-    colWidth = num;
-    rowHeight = colHeight;
-    rowWidth = num / x;
+function divEventListener() {
+    const divs = document.querySelectorAll('.row');
+    divs.forEach((e) => {
+        e.addEventListener('mousedown', changeColor);
+        e.addEventListener('mouseover', changeColor);
+    })
 }
 
-let gridValue;
+//add an eventListener for the all of the divs
+//add a function that changes the divs css only if the mouse in down
+
+
+
+
+
+let key;
+let gridValue = 16;
 let colHeight = 0;
 let colWidth = 0;
 let rowHeight = 0;
 let rowWidth = 0;
 let containerSize = getContainerSize();
+let activeButton;
+let mouseDown = false;
 generateColors();
-showRangeValue();
+generateCanvas(gridValue, containerSize);
+getRangeValue();
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+
+const genButton = document.querySelector('.gen-button');
+const clearButton = document.querySelector('.clear-button');
+const blackButton = document.querySelector('#colordiv1');
+const redButton = document.querySelector('#colordiv2');
+const yellowButton = document.querySelector('#colordiv3');
+const blueButton = document.querySelector('#colordiv4');
+const greenButton = document.querySelector('#colordiv5');
+const pinkButton = document.querySelector('#colordiv6');
+const rainbowButton = document.querySelector('#colordiv7');
+const whiteButton = document.querySelector('#colordiv8');
+
+genButton.addEventListener('click', () => {clearCanvas(); generateCanvas(gridValue, containerSize);} );
+clearButton.addEventListener('click', () => setWhite());
+blackButton.addEventListener('click',() =>  setActiveButton("black"));
+redButton.addEventListener('click', () =>  setActiveButton("red"));
+yellowButton.addEventListener('click', () =>  setActiveButton("yellow"));
+blueButton.addEventListener('click', () =>  setActiveButton("blue"));
+greenButton.addEventListener('click', () =>  setActiveButton("green"));
+pinkButton.addEventListener('click', () =>  setActiveButton("pink"));
+rainbowButton.addEventListener('click', () =>  setActiveButton("rainbow"));
+whiteButton.addEventListener('click', () =>  setActiveButton("white"));
+
+
